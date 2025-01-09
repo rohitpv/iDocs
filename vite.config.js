4f890add31920ce51189b5f15e43e87fac6644c6
Rohit Panchumarthy <rpanchumarthy@quadyster.com>
2025-01-09 18:39:38 +0530

    changed to allow socketIO proxy REST working

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://idocs.onrender.com',
+        changeOrigin: true,
+        ws: true
+      }
+    }
   }
 })
